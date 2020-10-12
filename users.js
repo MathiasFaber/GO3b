e8bc21096841fee74eec9d9ede1b7f382d3b5ded
MathiasFaber <makr20ah@student.cbs.dk>
2020-10-12 13:48:49 +0200

    hejsa

--- users.js
+++ users.js
@@ -1,12 +1,30 @@
 class user {
-    constructor(name, birthday, gender, location)
+    constructor(name, birthday, gender, location, email, password, uniqueUserID)
     {
         this.name = name;
         this.birthday = birthday;
         this.gender = gender;
         this.location = location;
+        this.email = email;
+        this.password = password;
+        this.uniqueUserID = uniqueUserID;
         //like funktion
+    };
+    
+    // Denne funktion skal skabe et unikt bruger-ID når person opretter sig i dating appen.
+    //Funktionen giver bruger 1 nr. 1 og bruger 2 nr 2 osv. 
+    var add = (function () {
+        var counter = 0;
+        return function () {counter += 1; return counter}
+      })();
+
+    
+    //Denne funktion skal lade brugerne like hinanden. Funktionen skal bruges senere i Match funktionen.
+    var like = function(){
+        
     }
+
+
 }
 
 class paymentuser extends user{
@@ -33,7 +51,7 @@ class freeUser extends user{
         super(name, birthday, gender, location)
     }
 }
-//hej, redigeret
+
 class interest extends user{
     constructor(name)
     {
