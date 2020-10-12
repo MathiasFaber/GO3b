ae65e2875fcf6227036271788427f1100b73b319
MathiasFaber <makr20ah@student.cbs.dk>
2020-10-12 13:14:14 +0200

    server committed

---
+++ GO3server.js
@@ -0,0 +1,21 @@
+var express = require('express')
+var cors = require('cors')
+var app = express()
+
+var add = (function () {
+  var counter = 0;
+  return function () {counter += 1; return counter}
+})();
+
+
+ 
+app.use(cors())
+ 
+app.get('/', function (req, res) {
+  res.json('Hello World!' + " " + add())
+})
+ 
+app.listen(3000, function () {
+  console.log('Server running on port 3000')
+})
+# GO3
